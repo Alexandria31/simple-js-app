5b13538792645b7f636f387e9117403ef23c8795
Alexandria31 <160250341+Alexandria31@users.noreply.github.com>
2024-04-19 21:17:11 +0900

    revised pokemon list
    following the array rule

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoritefood = 'pizza';
-document.write(favoritefood);
+let pokemonList = 
+
+[{name: "balbasaur", height: "0.7m", types: ['grass', 'poison']}, 
+{name: "charmander", height: "0.6m", types: "fire"},
+{name: "butterfree", height: "1.1m", types: ['bug, flying']}]
+ 
+console.log(pokemonList);
