22eea2c564af64ed646ce29bed574944d85b4d6e
Alexandria31 <160250341+Alexandria31@users.noreply.github.com>
2024-04-29 23:01:44 +0900

    exercise 1.3 revisions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,28 @@
-let pokemonList = 
 
-[{name: "balbasaur", height: "0.7m", types: ['grass', 'poison']}, 
-{name: "charmander", height: "0.6m", types: "fire"},
-{name: "butterfree", height: "1.1m", types: ['bug, flying']}]
- 
-console.log(pokemonList);
+let pokemonList = [
+    {
+      name: "balbasaur",
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: "charmander",
+      height: 0.6,
+      types: "fire"
+    },
+    {
+      name: "butterfree",
+      height: 1.1,
+      types: ['bug, flying']
+    }
+]
+
+for (let i=0; i<pokemonList.length; i++) {
+  if (pokemonList[i].height<0.7 ) {
+    document.write(pokemonList[i].name + "a small pokemon" + "<br>");
+  } else if (pokemonList[i].height>0.9) {
+    document.write(pokemonList[i].name + "a big pokemon" + "<br>");
+  } else {
+    document.write(pokemonList[i].name + "a medium pokemon" + "<br>");
+  }
+}
