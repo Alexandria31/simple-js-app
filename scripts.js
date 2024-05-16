3fc8e5508b88255ae78590746133ccdd0bdfdcfe
Alexandria31 <160250341+Alexandria31@users.noreply.github.com>
2024-05-17 07:07:49 +0900

    Exercise 1.5 revision request

--- scripts.js
+++ scripts.js
@@ -47,13 +47,3 @@ if (pokemon.height<0.7 ) {
   document.write(pokemon.name + "a medium pokemon" + "<br>");
 }
 })
-
-for (let i=0; i<pokemonList.length; i++) {
-  if (pokemonList[i].height<0.7 ) {
-    document.write(pokemonList[i].name + "a small pokemon" + "<br>");
-  } else if (pokemonList[i].height>0.9) {
-    document.write(pokemonList[i].name + "a big pokemon" + "<br>");
-  } else {
-    document.write(pokemon.name + "a medium pokemon" + "<br>");
-  }
-}
